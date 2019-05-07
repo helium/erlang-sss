3e082ba37b611ad0aa2ade71fb52a3ecea8070aa
Andrew Thompson <andrew@hijacked.us>
2019-05-07 12:03:01 -0700

    Fix build

--- c_src/compile.sh
+++ c_src/compile.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
 cd c_src
+if [ ! -d sss ]; then
+    git clone --recursive https://github.com/dsprenkels/sss.git
+fi
+
+if [ ! -f c_src/libsss.a ]; then
+    (cd sss ; make)
+fi
+
 make
