8933df4300ac2bf793f32e614b51a8f1fbf1ad27
Andrew Thompson <andrew@hijacked.us>
2019-10-06 15:35:05 -0700

    Allow override of NIF load path

--- src/erlang_sss.erl
+++ src/erlang_sss.erl
@@ -27,16 +27,22 @@ sss_combine_keyshares(_Shares, _K) ->
 %% Internal functions
 %%====================================================================
 init() ->
-    SoName = case code:priv_dir(?APPNAME) of
-        {error, bad_name} ->
-            case filelib:is_dir(filename:join(["..", priv])) of
-                true ->
-                    filename:join(["..", priv, ?LIBNAME]);
-                _ ->
-                    filename:join([priv, ?LIBNAME])
+    SoName =
+    case os:getenv("NIF_PATH") of
+        false ->
+            case code:priv_dir(?APPNAME) of
+                {error, bad_name} ->
+                    case filelib:is_dir(filename:join(["..", priv])) of
+                        true ->
+                            filename:join(["..", priv, ?LIBNAME]);
+                        _ ->
+                            filename:join([priv, ?LIBNAME])
+                    end;
+                Dir ->
+                    filename:join(Dir, ?LIBNAME)
             end;
-        Dir ->
-            filename:join(Dir, ?LIBNAME)
+        Path -> 
+            filename:join(Path, ?LIBNAME)
     end,
     erlang:load_nif(SoName, 0).
 
